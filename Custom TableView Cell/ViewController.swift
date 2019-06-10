ff4f4a0ef7ac577a0d6442e147d8f7dd8c2c4c06
waiphyoaungitman <eye03.rd@gmail.com>
2019-06-11 00:12:47 +0630

    Initial Commit

---
+++ Custom TableView Cell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Custom TableView Cell
+//
+//  Created by Wai Phyo  on 6/11/19.
+//  Copyright © 2019 waiphyoaung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
